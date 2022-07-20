0c96d975b754333922b64ade10711f9be94fe2f2
zahidalidev <m.zahidalidev@gmail.com>
2022-07-20 16:20:17 -0700

    Offline data persistence enable

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { addUser, getAllUsers } from "./app/services/user";
 
 export default function App() {
+  const handleUser = async () => {
+    try {
+      const data = await addUser({
+        name: "zahid2",
+      });
+
+      console.log("add: ", data);
+    } catch (error) {
+      console.log("errro: ", error);
+    }
+  };
+  const handleUserAll = async () => {
+    try {
+      const data = await getAllUsers();
+
+      console.log("all: ", data);
+    } catch (error) {
+      console.log("errro: ", error);
+    }
+  };
+  // handleUser();
+  handleUserAll();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity onPress={handleUser}>
+        <Text>Add User</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +38,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
