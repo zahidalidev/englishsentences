2126e90c165815b15df57bc60c57a9e90f01b1be
zahidalidev <m.zahidalidev@gmail.com>
2022-07-20 10:12:50 -0700

    dependencies installed

---
+++ app/config/app.js
@@ -0,0 +1,12 @@
+import Firebase from "firebase";
+import "firebase/firestore";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyATkn3pi_vlIajT-fIwdz79c-wXenuxmiY",
+  authDomain: "pandawantemp.firebaseapp.com",
+  projectId: "pandawantemp",
+  storageBucket: "pandawantemp.appspot.com",
+  messagingSenderId: "133114082609",
+  appId: "1:133114082609:web:dcf418a20b24d477526c10",
+  measurementId: "G-FJF58WWZXP",
+};
