cb23e3a918df589a8f28a790b60401871f489057
zahidalidev <m.zahidalidev@gmail.com>
2022-07-26 18:43:18 -0700

    order screen template

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import EditProduct from "./app/screens/EditProductScreen";
 import ProductList from "./app/screens/ProductListScreen";
 import ProductDetails from "./app/screens/ProductDetailsScreen";
 import EditProductVariant from "./app/screens/EditProductVariantScreen";
+import Orders from "./app/screens/OrdersScreen";
 
 // LogBox.ignoreAllLogs();
 
@@ -19,13 +20,14 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{ headerShown: false }}
-        initialRouteName="Home"
+        initialRouteName="Orders"
       >
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="EditCategory" component={EditCategory} />
         <Stack.Screen name="EditProduct" component={EditProduct} />
         <Stack.Screen name="ProductList" component={ProductList} />
         <Stack.Screen name="ProductDetails" component={ProductDetails} />
+        <Stack.Screen name="Orders" component={Orders} />
         <Stack.Screen
           name="EditProductVariant"
           component={EditProductVariant}
---
+++ app/screens/OrdersScreen.jsx
@@ -0,0 +1,142 @@
+import { useState } from "react";
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { RFPercentage } from "react-native-responsive-fontsize";
+import { Picker } from "@react-native-picker/picker";
+import { RadioButton } from "react-native-paper";
+
+import { Colors } from "../config/theme";
+import Input from "../components/common/Input";
+import Button from "../components/common/Button";
+
+const Orders = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor={Colors.white} barStyle="dark-content" />
+      <View style={styles.categHeadWrapper}>
+        <View style={styles.categHead}>
+          <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+            <MaterialCommunityIcons name="arrow-left" size={RFPercentage(3)} />
+          </TouchableOpacity>
+          <Text style={styles.categHeading}>My Orders</Text>
+        </View>
+      </View>
+
+      <View style={styles.radioContainer}>
+        <Text style={styles.radioHeading}>AVAILABILITY</Text>
+        <View style={styles.radioWrapper}>
+          <Text style={styles.radioLabel}>Available</Text>
+          <RadioButton
+            value="first"
+            status={"checked"}
+            // onPress={() => setChecked("first")}
+          />
+        </View>
+        <View style={styles.radioWrapper}>
+          <Text style={styles.radioLabel}>Unavailable</Text>
+          <RadioButton
+            value="first"
+            status={"checked"}
+            // onPress={() => setChecked("first")}
+          />
+        </View>
+      </View>
+
+      <View style={styles.radioContainer}>
+        <Text style={styles.radioHeading}>WEIGHED</Text>
+        <View style={styles.radioWrapper}>
+          <Text style={styles.radioLabel}>yes</Text>
+          <RadioButton
+            value="first"
+            status={"checked"}
+            // onPress={() => setChecked("first")}
+          />
+        </View>
+        <View style={styles.radioWrapper}>
+          <Text style={styles.radioLabel}>No</Text>
+          <RadioButton
+            value="first"
+            status={"checked"}
+            // onPress={() => setChecked("first")}
+          />
+        </View>
+      </View>
+
+      <View style={styles.btnwrapper}>
+        <Button name="Save Changes" width="90%" />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.lightGrey,
+    alignItems: "center",
+  },
+
+  categHeadWrapper: {
+    width: "100%",
+    backgroundColor: Colors.white,
+    padding: RFPercentage(2),
+    elevation: 2,
+  },
+
+  categHead: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "67%",
+  },
+
+  categHeading: {
+    fontSize: RFPercentage(2.8),
+    fontWeight: "bold",
+  },
+
+  btnwrapper: {
+    width: "100%",
+    alignItems: "center",
+    position: "absolute",
+    bottom: RFPercentage(3),
+  },
+
+  radioContainer: {
+    width: "95%",
+    marginTop: RFPercentage(4),
+    padding: RFPercentage(2),
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.white,
+    borderRadius: 5,
+  },
+
+  radioHeading: {
+    fontSize: RFPercentage(2),
+    color: Colors.grey,
+    fontWeight: "500",
+    alignSelf: "flex-start",
+  },
+
+  radioWrapper: {
+    flexDirection: "row",
+    width: "90%",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: RFPercentage(1),
+  },
+
+  radioLabel: {
+    fontSize: RFPercentage(2.5),
+    fontWeight: "500",
+  },
+});
+
+export default Orders;
